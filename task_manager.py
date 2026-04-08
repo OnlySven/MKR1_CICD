8278a5e26f7337226401292f6396ba5c4478eb47
Pavlo Svatyj <pavelsvatyj@gmail.com>
2026-04-08 15:32:47 +0300

    Додано метод завантаження завдань із файлу

--- task_manager.py
+++ task_manager.py
@@ -10,3 +10,16 @@ class TaskManager:
         self.tasks = []
         self.next_id = 1
         self._load_tasks()
+
+    def _load_tasks(self):
+        """Завантажує завдання з файлу і перетворює їх в об'єкти Task."""
+        if os.path.exists(self.filename):
+            with open(self.filename, 'r', encoding='utf-8') as f:
+                try:
+                    data = json.load(f)
+                    # Використовуємо метод класу Task для створення об'єктів
+                    self.tasks = [Task.from_dict(t) for t in data]
+                    if self.tasks:
+                        self.next_id = max(t.task_id for t in self.tasks) + 1
+                except json.JSONDecodeError:
+                    self.tasks = []
