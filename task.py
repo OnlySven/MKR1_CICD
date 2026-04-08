1c0276ed5b800346d22483e99191bfa111bde5fd
Pavlo Svatyj <pavelsvatyj@gmail.com>
2026-04-08 15:30:57 +0300

    Додано метод from_dict для створення об'єкта завдання зі словника при завантаженні з JSON

--- task.py
+++ task.py
@@ -19,3 +19,13 @@ class Task:
             'priority': self.priority,
             'creation_date': self.creation_date
         }
+    
+    @classmethod
+    def from_dict(cls, data: dict):
+        """Створює об'єкт завдання зі словника (при завантаженні з JSON)."""
+        return cls(
+            task_id=data['id'],
+            description=data['description'],
+            priority=data['priority'],
+            creation_date=data['creation_date']
+        )
